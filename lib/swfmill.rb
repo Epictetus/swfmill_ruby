205181aa8fde9ab1a1d40afb8fe29430339d4f83
koumiya <koumiya.katame@gmail.com>
2010-01-29 17:49:43 +0900

    First aid for swfmill version 0.3.0

--- lib/swfmill.rb
+++ lib/swfmill.rb
@@ -4,7 +4,7 @@ module SwfmillUtil
 
   class Swfmill
 
-    def self.xml2swf(xml, option = "-e cp932")
+    def self.xml2swf(xml, option = "")#, option = "-e cp932")
       IO.popen("#{SWFMILL} #{option} xml2swf stdin", "r+") do |io|
         io.write xml
         io.close_write
@@ -12,7 +12,7 @@ module SwfmillUtil
       end
     end
 
-    def self.swf2xml(swf, option = "-e cp932")
+    def self.swf2xml(swf, option = "")#, option = "-e cp932")
       IO.popen("#{SWFMILL} #{option} swf2xml stdin", "r+") do |io|
         io.write swf
         io.close_write
